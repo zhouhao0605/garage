8c4cfaa40cc346dc83d17febec1874cac77948c9
Nicole Ng <shinyinn@usc.edu>
2020-09-15 12:58:36 -0700

    Update InProgressEpisode with episode_info

--- src/garage/sampler/_dtypes.py
+++ src/garage/sampler/_dtypes.py
@@ -7,6 +7,7 @@ from garage import EpisodeBatch, StepType
 
 
 class InProgressEpisode:
+
     """An in-progress episode.
 
     Compared to EpisodeBatch, this datatype does less checking, only
@@ -18,13 +19,25 @@ class InProgressEpisode:
             in.
         initial_observation (np.ndarray): The first observation. If None, the
             environment will be reset to generate this observation.
-
+    Raises:
+        ValueError: if either initial_observation and episode_info is passed in
+            but not the other. Either both or neither should be passed in.
     """
 
-    def __init__(self, env, initial_observation=None):
-        self.env = env
+    def __init__(self, env, initial_observation=None, episode_info=None):
+        if initial_observation is None and episode_info is not None:
+            raise ValueError(
+                'Initial observation and episode info must be both or '
+                'neither provided, but only episode info was passed in')
+        if initial_observation is not None and episode_info is None:
+            raise ValueError(
+                'Initial observation and episode info must be both or '
+                'neither provided, but only initial observation was passed in')
+
         if initial_observation is None:
-            initial_observation = env.reset()[0]
+            initial_observation, episode_info = env.reset()
+        self.env = env
+        self.episode_info = episode_info
         self.observations = [initial_observation]
         self.actions = []
         self.rewards = []
@@ -68,11 +81,15 @@ class InProgressEpisode:
         assert len(self.rewards) > 0
         env_infos = dict(self.env_infos)
         agent_infos = dict(self.agent_infos)
+        episode_infos = dict(self.episode_info)
         for k, v in env_infos.items():
             env_infos[k] = np.asarray(v)
         for k, v in agent_infos.items():
             agent_infos[k] = np.asarray(v)
-        return EpisodeBatch(env_spec=self.env.spec,
+        for k, v in episode_infos.items():
+            episode_infos[k] = np.asarray([v])
+        return EpisodeBatch(episode_infos=episode_infos,
+                            env_spec=self.env.spec,
                             observations=np.asarray(self.observations[:-1]),
                             last_observations=np.asarray([self.last_obs]),
                             actions=np.asarray(self.actions),
@@ -81,7 +98,6 @@ class InProgressEpisode:
                                                   dtype=StepType),
                             env_infos=env_infos,
                             agent_infos=agent_infos,
-                            episode_infos=dict(),
                             lengths=np.asarray([len(self.rewards)], dtype='l'))
 
     @property
